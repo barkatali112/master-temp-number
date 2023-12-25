41e8fdbd1b31a449a146eacc056cb257794fed83
MASTER-HACK <149458168+Xzhackyy@users.noreply.github.com>
2023-12-25 10:54:30 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+git branch
+git checkout -b master
+git add run.sh keyfile
+git commit -m "Add decryption step to run.sh"
+
 git show master:keyfile > keyfile
 # Decrypt and run the script
 openssl aes-256-cbc -d -in number.enc -out number.sh -pass file:keyfile -pbkdf2 -iter 100000
